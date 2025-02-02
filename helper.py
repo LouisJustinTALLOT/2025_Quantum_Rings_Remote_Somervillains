86a6e934864e54ea141ed5057bc62a18cb28d78a
Florent Pollet <flo1.raymond@gmail.com>
2025-02-01 23:21:57 -0500

    progress: test code from online but only 15 works

---
+++ helper.py
@@ -0,0 +1,382 @@
+
+""" Imports from qiskit"""
+from QuantumRingsLib import QuantumCircuit, QuantumRegister, ClassicalRegister, job_monitor
+# from qiskit import execute, IBMQ
+# from qiskit import BasicAer
+
+import sys
+
+""" Imports to Python functions """
+import math
+import array
+import fractions
+import numpy as np
+
+""" Function to check if N is of type q^p"""
+def check_if_power(N):
+    """ Check if N is a perfect power in O(n^3) time, n=ceil(logN) """
+    b=2
+    while (2**b) <= N:
+        a = 1
+        c = N
+        while (c-a) >= 2:
+            m = int( (a+c)/2 )
+
+            if (m**b) < (N+1):
+                p = int( (m**b) )
+            else:
+                p = int(N+1)
+
+            if int(p) == int(N):
+                print('N is {0}^{1}'.format(int(m),int(b)) )
+                return True
+
+            if p<N:
+                a = int(m)
+            else:
+                c = int(m)
+        b=b+1
+
+    return False
+
+""" Function to get the value a ( 1<a<N ), such that a and N are coprime. Starts by getting the smallest a possible
+    This normally can be done fully randomly, we just did it like this for thr user (professor) to have control 
+    over the a value that gets selected """
+def get_value_a(N):
+
+    """ ok defines if user wants to used the suggested a (if ok!='0') or not (if ok=='0') """
+    ok='0'
+
+    """ Starting with a=2 """
+    a=2
+
+    """ Get the smallest a such that a and N are coprime"""
+    while math.gcd(a,N)!=1:
+        a=a+1
+
+    """ Store it as the smallest a possible """
+    smallest_a = a
+
+    """ Ask user if the a found is ok, if not, then increment and find the next possibility """  
+    ok = input('Is the number {0} ok for a? Press 0 if not, other number if yes: '.format(a))
+    if ok=='0':
+        if(N==3):
+            print('Number {0} is the only one you can use. Using {1} as value for a\n'.format(a,a))
+            return a
+        a=a+1
+
+    """ Cycle to find all possibilities for a not counting the smallest one, until user says one of them is ok """
+    while ok=='0':
+        
+        """ Get a coprime with N """
+        while math.gcd(a,N)!=1:
+            a=a+1
+    
+        """ Ask user if ok """
+        ok = input('Is the number {0} ok for a? Press 0 if not, other number if yes: '.format(a))
+
+        """ If user says it is ok, then exit cycle, a has been found """
+        if ok!='0':
+            break
+        
+        """ If user says it is not ok, increment a and check if are all possibilites checked.  """
+        a=a+1
+
+        """ If all possibilities for a are rejected, put a as the smallest possible value and exit cycle """
+        if a>(N-1):
+            print('You rejected all options for value a, selecting the smallest one\n')
+            a=smallest_a
+            break
+
+    """ Print the value that is used as a """
+    print('Using {0} as value for a\n'.format(a))
+
+    return a
+
+""" Function to apply the continued fractions to find r and the gcd to find the desired factors"""
+def get_factors(x_value,t_upper,N,a):
+
+    if x_value<=0:
+        print('x_value is <= 0, there are no continued fractions\n')
+        return False, -1, -1  
+
+    print('Running continued fractions for this case\n')
+
+    """ Calculate T and x/T """
+    T = pow(2,t_upper)
+
+    x_over_T = x_value/T
+
+    """ Cycle in which each iteration corresponds to putting one more term in the
+    calculation of the Continued Fraction (CF) of x/T """
+
+    """ Initialize the first values according to CF rule """
+    i=0
+    b = array.array('i')
+    t = array.array('f')
+
+    b.append(math.floor(x_over_T))
+    t.append(x_over_T - b[i])
+
+    while i>=0:
+
+        """From the 2nd iteration onwards, calculate the new terms of the CF based
+        on the previous terms as the rule suggests"""
+
+        if i>0:
+            b.append( math.floor( 1 / (t[i-1]) ) ) 
+            t.append( ( 1 / (t[i-1]) ) - b[i] )
+
+        """ Calculate the CF using the known terms """
+
+        aux = 0
+        j=i
+        while j>0:    
+            aux = 1 / ( b[j] + aux )      
+            j = j-1
+        
+        aux = aux + b[0]
+
+        """Get the denominator from the value obtained"""
+        frac = fractions.Fraction(aux).limit_denominator()
+        den=frac.denominator
+
+        print('Approximation number {0} of continued fractions:'.format(i+1))
+        print("Numerator:{0} \t\t Denominator: {1}\n".format(frac.numerator,frac.denominator))
+
+        """ Increment i for next iteration """
+        i=i+1
+
+        if (den%2) == 1:
+            if i>=15:
+                print('Returning because have already done too much tries')
+                return False, -1, -1  
+            print('Odd denominator, will try next iteration of continued fractions\n')
+            continue
+    
+        """ If denominator even, try to get factors of N """
+
+        """ Get the exponential a^(r/2) """
+
+        exponential = 0
+
+        if den<1000:
+            exponential=pow(a , (den/2))
+        
+        """ Check if the value is too big or not """
+        if math.isinf(exponential)==1 or exponential>1000000000:
+            print('Denominator of continued fraction is too big!\n')
+            aux_out = input('Input number 1 if you want to continue searching, other if you do not: ')
+            if aux_out != '1':
+                return False, -1, -1  
+            else:
+                continue
+
+        """If the value is not to big (infinity), then get the right values and
+        do the proper gcd()"""
+
+        putting_plus = int(exponential + 1)
+
+        putting_minus = int(exponential - 1)
+    
+        one_factor = math.gcd(putting_plus,N)
+        other_factor = math.gcd(putting_minus,N)
+    
+        """ Check if the factors found are trivial factors or are the desired
+        factors """
+
+        if one_factor==1 or one_factor==N or other_factor==1 or other_factor==N:
+            print('Found just trivial factors, not good enough\n')
+            """ Check if the number has already been found, use i-1 because i was already incremented """
+            if t[i-1]==0:
+                print('The continued fractions found exactly x_final/(2^(2n)) , leaving funtion\n')
+                return False, -1, -1  
+            if i<15:
+                aux_out = input('Input number 1 if you want to continue searching, other if you do not: ')
+                if aux_out != '1':
+                    return False, -1, -1  
+            else:
+                """ Return if already too much tries and numbers are huge """ 
+                print('Returning because have already done too many tries\n')
+                return False, -1, -1     
+        else:
+            print('The factors of {0} are {1} and {2}\n'.format(N,one_factor,other_factor))
+            print('Found the desired factors!\n')
+            return True, one_factor, other_factor
+
+"""Functions that calculate the modular inverse using Euclid's algorithm"""
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+""" Function to create the QFT """
+def create_QFT(circuit,up_reg,n,with_swaps):
+    i=n-1
+    """ Apply the H gates and Cphases"""
+    """ The Cphases with |angle| < threshold are not created because they do 
+    nothing. The threshold is put as being 0 so all CPhases are created,
+    but the clause is there so if wanted just need to change the 0 of the
+    if-clause to the desired value """
+    while i>=0:
+        circuit.h(up_reg[i])        
+        j=i-1  
+        while j>=0:
+            if (np.pi)/(pow(2,(i-j))) > 0:
+                circuit.cu1( (np.pi)/(pow(2,(i-j))) , up_reg[i] , up_reg[j] )
+                j=j-1   
+        i=i-1  
+
+    """ If specified, apply the Swaps at the end """
+    if with_swaps==1:
+        i=0
+        while i < ((n-1)/2):
+            circuit.swap(up_reg[i], up_reg[n-1-i])
+            i=i+1
+
+""" Function to create inverse QFT """
+def create_inverse_QFT(circuit,up_reg,n,with_swaps):
+    """ If specified, apply the Swaps at the beggining"""
+    if with_swaps==1:
+        i=0
+        while i < ((n-1)/2):
+            circuit.swap(up_reg[i], up_reg[n-1-i])
+            i=i+1
+    
+    """ Apply the H gates and Cphases"""
+    """ The Cphases with |angle| < threshold are not created because they do 
+    nothing. The threshold is put as being 0 so all CPhases are created,
+    but the clause is there so if wanted just need to change the 0 of the
+    if-clause to the desired value """
+    i=0
+    while i<n:
+        circuit.h(up_reg[i])
+        if i != n-1:
+            j=i+1
+            y=i
+            while y>=0:
+                 if (np.pi)/(pow(2,(j-y))) > 0:
+                    circuit.cu1( - (np.pi)/(pow(2,(j-y))) , up_reg[j] , up_reg[y] )
+                    y=y-1   
+        i=i+1
+
+"""Function that calculates the angle of a phase shift in the sequential QFT based on the binary digits of a."""
+"""a represents a possile value of the classical register"""
+def getAngle(a, N):
+    """convert the number a to a binary string with length N"""
+    s=bin(int(a))[2:].zfill(N) 
+    angle = 0
+    for i in range(0, N):
+        """if the digit is 1, add the corresponding value to the angle"""
+        if s[N-1-i] == '1': 
+            angle += math.pow(2, -(N-i))
+    angle *= np.pi
+    return angle
+
+"""Function that calculates the array of angles to be used in the addition in Fourier Space"""
+def getAngles(a,N):
+    s=bin(int(a))[2:].zfill(N) 
+    angles=np.zeros([N])
+    for i in range(0, N):
+        for j in range(i,N):
+            if s[j]=='1':
+                angles[N-i-1]+=math.pow(2, -(j-i))
+        angles[N-i-1]*=np.pi
+    return angles
+
+"""Creation of a doubly controlled phase gate"""
+def ccphase(circuit, angle, ctl1, ctl2, tgt):
+    circuit.cu1(angle/2,ctl1,tgt)
+    circuit.cx(ctl2,ctl1)
+    circuit.cu1(-angle/2,ctl1,tgt)
+    circuit.cx(ctl2,ctl1)
+    circuit.cu1(angle/2,ctl2,tgt)
+
+"""Creation of the circuit that performs addition by a in Fourier Space"""
+"""Can also be used for subtraction by setting the parameter inv to a value different from 0"""
+def phiADD(circuit, q, a, N, inv):
+    angle=getAngles(a,N)
+    for i in range(0,N):
+        if inv==0:
+            circuit.u1(angle[i],q[i])
+            """addition"""
+        else:
+            circuit.u1(-angle[i],q[i])
+            """subtraction"""
+
+"""Single controlled version of the phiADD circuit"""
+def cphiADD(circuit, q, ctl, a, n, inv):
+    angle=getAngles(a,n)
+    for i in range(0,n):
+        if inv==0:
+            circuit.cu1(angle[i],ctl,q[i])
+        else:
+            circuit.cu1(-angle[i],ctl,q[i])
+        
+"""Doubly controlled version of the phiADD circuit"""
+def ccphiADD(circuit,q,ctl1,ctl2,a,n,inv):
+    angle=getAngles(a,n)
+    for i in range(0,n):
+        if inv==0:
+            ccphase(circuit,angle[i],ctl1,ctl2,q[i])
+        else:
+            ccphase(circuit,-angle[i],ctl1,ctl2,q[i])
+        
+"""Circuit that implements doubly controlled modular addition by a"""
+def ccphiADDmodN(circuit, q, ctl1, ctl2, aux, a, N, n):
+    ccphiADD(circuit, q, ctl1, ctl2, a, n, 0)
+    phiADD(circuit, q, N, n, 1)
+    create_inverse_QFT(circuit, q, n, 0)
+    circuit.cx(q[n-1],aux)
+    create_QFT(circuit,q,n,0)
+    cphiADD(circuit, q, aux, N, n, 0)
+    
+    ccphiADD(circuit, q, ctl1, ctl2, a, n, 1)
+    create_inverse_QFT(circuit, q, n, 0)
+    circuit.x(q[n-1])
+    circuit.cx(q[n-1], aux)
+    circuit.x(q[n-1])
+    create_QFT(circuit,q,n,0)
+    ccphiADD(circuit, q, ctl1, ctl2, a, n, 0)
+
+"""Circuit that implements the inverse of doubly controlled modular addition by a"""
+def ccphiADDmodN_inv(circuit, q, ctl1, ctl2, aux, a, N, n):
+    ccphiADD(circuit, q, ctl1, ctl2, a, n, 1)
+    create_inverse_QFT(circuit, q, n, 0)
+    circuit.x(q[n-1])
+    circuit.cx(q[n-1],aux)
+    circuit.x(q[n-1])
+    create_QFT(circuit, q, n, 0)
+    ccphiADD(circuit, q, ctl1, ctl2, a, n, 0)
+    cphiADD(circuit, q, aux, N, n, 1)
+    create_inverse_QFT(circuit, q, n, 0)
+    circuit.cx(q[n-1], aux)
+    create_QFT(circuit, q, n, 0)
+    phiADD(circuit, q, N, n, 0)
+    ccphiADD(circuit, q, ctl1, ctl2, a, n, 1)
+
+"""Circuit that implements single controlled modular multiplication by a"""
+def cMULTmodN(circuit, ctl, q, aux, a, N, n):
+    create_QFT(circuit,aux,n+1,0)
+    for i in range(0, n):
+        ccphiADDmodN(circuit, aux, q[i], ctl, aux[n+1], (2**i)*a % N, N, n+1)
+    create_inverse_QFT(circuit, aux, n+1, 0)
+
+    for i in range(0, n):
+        circuit.cswap(ctl,q[i],aux[i])
+
+    a_inv = modinv(a, N)
+    create_QFT(circuit, aux, n+1, 0)
+    i = n-1
+    while i >= 0:
+        ccphiADDmodN_inv(circuit, aux, q[i], ctl, aux[n+1], math.pow(2,i)*a_inv % N, N, n+1)
+        i -= 1
+    create_inverse_QFT(circuit, aux, n+1, 0)
--- shor.py
+++ shor.py
@@ -6,11 +6,17 @@ import semiprimes
 import time
 import json
 import matplotlib.pyplot as plt
+from QuantumRingsLib import QuantumRegister, AncillaRegister, ClassicalRegister, QuantumCircuit
+
+
+# https://github.com/tiagomsleao/ShorAlgQiskit/blob/master/Shor_Normal_QFT.py
 
 from fractions import Fraction
+# from helper import getAngle, cMULTmodN, get_factors
+from helper_norm import getAngles, cMULTmodN, get_factors, create_inverse_QFT
 
 
-shots = 1024
+shots = 256
 
 
 def modular_exponentiation(base, exp, mod):
@@ -31,6 +37,9 @@ def iqft_cct(qc, b, n):
         qc.h(b[i])
     qc.barrier()
     return
+
+
+# https://github.com/qiskit-community/qiskit-community-tutorials/blob/master/algorithms/shor_algorithm.ipynb
 
 
 def extract_period_from_counts(counts, a, N):
@@ -80,61 +89,140 @@ def shors_algorithm(N):
     
     a = np.random.randint(2, N)
 
+    # a = 4
+    # N = 15
+
     if math.gcd(a, N) != 1:
         print("GCD is not 1, returning GCD")
         return math.gcd(a, N)
-    
+        
     n = int(np.ceil(np.log2(N)))
-    qr = QuantumCircuit(2*n, 2*n)
 
-    print(f'Looking for N={N} with a={a}, n={n} (total qubits: {2*n})')
 
-    for qubit in range(n):
-        qr.h(qubit)
+    # """auxilliary quantum register used in addition and multiplication"""
+    # aux = QuantumRegister(n+2)
+    # """single qubit where the sequential QFT is performed"""
+    # up_reg = QuantumRegister(1)
+    # """quantum register where the multiplications are made"""
+    # down_reg = QuantumRegister(n)
+    # """classical register where the measured values of the sequential QFT are stored"""
+    # up_classic = ClassicalRegister(2*n)
+    # """classical bit used to reset the state of the top qubit to 0 if the previous measurement was 1"""
+    # c_aux = ClassicalRegister(1)
+    # print(f'Looking for N={N} with a={a}, n={n} (total qubits: {2*n})')
 
-    qr.barrier()
+    # circuit = QuantumCircuit(down_reg , up_reg , aux, up_classic, c_aux)
 
-    for qubit in range(n):
-        qr.cu1(2*np.pi*a**(2**qubit)/N, qubit, n) # TODO : Find a way to remove that a**(2**qubit)?
+    # """ Initialize down register to 1"""
+    # circuit.x(down_reg[0])
 
-    
-    # mod_exp = 1  # a^(2^0) mod N
-    # for qubit in range(n):
-    #     phase = (2 * np.pi * mod_exp) / N
-    #     qr.p(phase, n)  # Replace cu1 with equivalent phase gate
-    #     mod_exp = modular_exponentiation(a, 2**qubit, N)  # Compute a^(2^qubit) mod N
+    # """ Cycle to create the Sequential QFT, measuring qubits and applying the right gates according to measurements """
+    # for i in range(0, 2*n):
+    #     """reset the top qubit to 0 if the previous measurement was 1"""
+    #     circuit.x(up_reg).c_if(c_aux, 1)
+    #     circuit.h(up_reg)
+    #     cMULTmodN(circuit, up_reg[0], down_reg, aux, a**(2**(2*n-1-i)), N, n)
+    #     """cycle through all possible values of the classical register and apply the corresponding conditional phase shift"""
+    #     for j in range(0, 2**i):
+    #         """the phase shift is applied if the value of the classical register matches j exactly"""
+    #         circuit.u1(getAngle(j, i), up_reg[0]).c_if(up_classic, j)
+    #     circuit.h(up_reg)
+    #     circuit.measure(up_reg[0], up_classic[i])
+    #     circuit.measure(up_reg[0], c_aux[0])
 
-    qr.barrier()
-    iqft_cct(qr, range(n), n)
 
-    qr.barrier()
+    """auxilliary quantum register used in addition and multiplication"""
+    aux = QuantumRegister(n+2)
+    """quantum register where the sequential QFT is performed"""
+    up_reg = QuantumRegister(2*n)
+    """quantum register where the multiplications are made"""
+    down_reg = QuantumRegister(n)
+    """classical register where the measured values of the QFT are stored"""
+    up_classic = ClassicalRegister(2*n)
 
-    qr.measure(range(n), range(n))
+    """ Create Quantum Circuit """
+    circuit = QuantumCircuit(down_reg , up_reg , aux, up_classic)
+
+    """ Initialize down register to 1 and create maximal superposition in top register """
+    circuit.h(up_reg)
+    circuit.x(down_reg[0])
+
+    """ Apply the multiplication gates as showed in the report in order to create the exponentiation """
+    for i in range(0, 2*n):
+        cMULTmodN(circuit, up_reg[i], down_reg, aux, int(pow(a, pow(2, i))), N, n)
+
+    """ Apply inverse QFT """
+    create_inverse_QFT(circuit, up_reg, 2*n ,1)
+
+    """ Measure the top qubits, to get x value"""
+    circuit.measure(up_reg,up_classic)
 
     # capture stdout to save qr circuit when calling qr.draw
-    import sys
+    if False:
+        import sys
 
-    from io import StringIO
-    old_stdout = sys.stdout
-    sys.stdout = StringIO()
-    qr.draw()
+        from io import StringIO
+        old_stdout = sys.stdout
+        sys.stdout = StringIO()
+        circuit.draw(fold=-1)
 
-    # save to file
-    with open(f'shor_circuit_{N}.txt', 'w') as f:
-        f.write(sys.stdout.getvalue())
+        # save to file
+        with open(f'shor_circuit_{N}.txt', 'w') as f:
+            f.write(sys.stdout.getvalue())
 
-    sys.stdout = old_stdout
+        sys.stdout = old_stdout
+
+    # format depth and width as as string with explanations
+    print(f"circuit depth: {circuit.depth()}, circuit width: {circuit.width()}")
+
+    # exit()
 
 
-    qc = qr
+    qc = circuit
 
     backend = QR_secrets.backend
     job = backend.run(qc, shots=shots)
     job_monitor(job)
     result = job.result()
     counts = result.get_counts()
+
+    measured_values = list(counts.keys())  # Get binary strings
+    probabilities = list(counts.values())  # Get occurrence counts
+
+    print(measured_values, probabilities)
     
-    r = extract_period_from_counts(counts, a, N)
+    # r = extract_period_from_counts(counts, a, N)
+    # exit()
+
+    """ Initialize this variable """
+    prob_success=0
+    sim_result = result
+    counts_result = list(counts.values())
+    number_shots = shots
+    """ For each simulation result, print proper info to user and try to calculate the factors of N"""
+    i=0
+    while i < len(counts_result):
+
+        """ Get the x_value from the final state qubits """
+        all_registers_output = list(sim_result.get_counts().keys())[i]
+        output_desired = all_registers_output#.split(" ")[1]
+        x_value = int(output_desired, 2)
+        prob_this_result = 100 * ( int( list(sim_result.get_counts().values())[i] ) ) / (number_shots)
+
+        print("------> Analysing result {0}. This result happened in {1:.4f} % of all cases\n".format(output_desired,prob_this_result))
+
+        """ Print the final x_value to user """
+        print('In decimal, x_final value for this result is: {0}\n'.format(x_value))
+
+        """ Get the factors using the x value obtained """   
+        success, f1, f2 = get_factors(int(x_value),int(2*n),int(N),int(a))
+
+        if success==True:
+            prob_success = prob_success + prob_this_result
+            print("FOUND FACTORS: {0} and {1}\n".format(f1,f2))
+            return f1, f2
+
+        i=i+1
 
     # measured_value = int(list(counts.keys())[0], 2)
 
@@ -147,10 +235,10 @@ def shors_algorithm(N):
     #         print(a, N)
     #         break
 
-    if r % 2 == 0 and (a**(r//2)+1)%N != 0:
-        factor1 = math.gcd(a**(r//2)-1, N)
-        factor2 = math.gcd(a**(r//2)+1, N)
-        return factor1, factor2
+    # if r % 2 == 0 and (a**(r//2)+1)%N != 0:
+    #     factor1 = math.gcd(a**(r//2)-1, N)
+    #     factor2 = math.gcd(a**(r//2)+1, N)
+    #     return factor1, factor2
 
     return None
 
